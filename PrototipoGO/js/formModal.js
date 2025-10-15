c93e03e569486bc05a1484e3f6abcaacf08c91aa
Felipe-Victorino <felipe.flvictorino@gmail.com>
2025-10-15 00:17:40 -0300

    prepare(*): preparando pro merge

--- PrototipoGO/js/formModal.js
+++ PrototipoGO/js/formModal.js
@@ -6,6 +6,21 @@ function loadFormResults() {
   let formList = document.querySelectorAll("#form-info");
 
   let modalbody = document.querySelector(".modal-body");
+  if (isListEmpty(formList) == true) {
+    console.log("Lista vazia");
+    return;
+  }
+
+  let textvalues = "";
+
+  for (let index = 0; index < formList.length; index++) {
+    let element = formList[index];
+    console.log(element.innerHTML);
+  }
 
   console.log(formList);
+}
+
+function isListEmpty(list) {
+  return list.length == 0;
 }
