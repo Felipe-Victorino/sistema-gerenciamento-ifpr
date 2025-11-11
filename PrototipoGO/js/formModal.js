683a1cf3dedfc99e8ea3ca2230d0202dbf155fdc
Felipe-Victorino <felipe.flvictorino@gmail.com>
2025-11-11 19:47:02 -0300

    fix(img) imagens menos pesadas utilizadas agora

--- PrototipoGO/js/formModal.js
+++ PrototipoGO/js/formModal.js
@@ -11,7 +11,12 @@ function loadFormResults() {
     console.log("Lista vazia");
     return;
   }
-
+  console.log("Success!");
+  if (isListValuesEmpty(formList) == true) {
+    console.log("Conteúdo da Lista vazio");
+    return;
+  }
+  console.log("Success!");
   let textvalues = "";
 
   let selectStrings = [
@@ -23,13 +28,30 @@ function loadFormResults() {
   for (let index = 0; index < formList.length; index++) {
     let elementForm = formList[index];
     let elementInfo = infoList[index];
-    console.log(elementInfo.innerHTML + elementForm.value);
-    textvalues =
-      textvalues + elementInfo.innerHTML + " " + elementForm.value + "<br>";
+    console.log(elementForm.value);
+    if (index == 2) {
+      textvalues =
+        textvalues +
+        elementInfo.innerHTML +
+        " " +
+        selectStrings[elementForm.value] +
+        "<br>";
+    } else {
+      textvalues =
+        textvalues + elementInfo.innerHTML + " " + elementForm.value + "<br>";
+    }
   }
-  console.log(textvalues);
 
   modalbody.innerHTML = textvalues;
+}
+
+function isListValuesEmpty(list) {
+  list.forEach((element) => {
+    if (element.value == null || element.value == "") {
+      return true;
+    }
+  });
+  return false;
 }
 
 function isListEmpty(list) {
