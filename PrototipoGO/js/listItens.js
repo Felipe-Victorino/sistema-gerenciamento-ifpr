d8c2f8a522d3ea3af3bf28d14cfcd23e40a731bb
Felipe-Victorino <felipe.flvictorino@gmail.com>
2025-11-17 19:51:39 -0300

    style(catalogo): alterei as cores utilizadas no catálogo para o padrão

--- PrototipoGO/js/listItens.js
+++ PrototipoGO/js/listItens.js
@@ -1,9 +1,10 @@
- const itens = [
-        { id: 1, nome: "calculadora", qtde: 4 },
-        { id: 2, nome: "caneta", qtde: 4 },
-        { id: 2, nome: "lapis", qtde: 3 },
-        { id: 4, nome: "grampeador", qtde: 4 },
-      ];
+const itens = [
+  { id: 1, nome: "Calculadora", qtde: 5 },
+  { id: 2, nome: "Caneta", qtde: 25 },
+  { id: 2, nome: "Lápis", qtde: 20 },
+  { id: 4, nome: "Grampeador", qtde: 4 },
+  {id: 5, nome: "Papel A4", qtde: 50}
+];
 
 const lista = document.getElementById("lista-itens");
 const campoFiltro = document.getElementById("campo-filtro");
@@ -12,27 +13,33 @@ const btnBuscar = document.getElementById("btn-buscar");
 function renderizarItensList(filtro = "") {
   lista.innerHTML = ""; // limpa antes de renderizar
 
-        itens
-          .filter((item) =>
-            item.nome.toLowerCase().includes(filtro.toLowerCase())
-          )
-          .forEach((item) => {
-            const li = document.createElement("div");
-            li.className = "list-group-item w-100 d-flex justify-content-between ";
+  itens
+    .filter((item) => item.nome.toLowerCase().includes(filtro.toLowerCase()))
+    .forEach((item) => {
+      const li = document.createElement("div");
+      li.className = "list-group-item w-100 d-flex justify-content-between ";
 
-            const nome = document.createElement('span');
-            nome.textContent = item.nome;
-            nome.className = "conteudo ms-1"
+      const nome = document.createElement("span");
+      nome.textContent = item.nome;
+      nome.className = "conteudo ms-1";
 
-            const estoque = document.createElement('span');
-            estoque.textContent = item.qtde;
-            nome.className = "conteudo align-content-center"
+      const estoque = document.createElement("span");
+      estoque.textContent = item.qtde;
+      nome.className = "conteudo align-content-center";
 
       const add = document.createElement("button");
-      add.textContent = "adicionar";
+      add.textContent = "Adicionar";
       add.className = "btn ms-auto";
 
-      li.append(nome, estoque, add);
+      const rem = document.createElement("button");
+      rem.textContent = "Remover";
+      rem.className = "btn ms-auto";
+
+      const edi = document.createElement("button");
+      edi.textContent = "Editar";
+      edi.className = "btn ms-auto";
+
+      li.append(nome, estoque, add, rem, edi);
       lista.appendChild(li);
     });
 }
